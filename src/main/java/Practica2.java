ae45c448b4155543d8d93c3e98819eb01bebe458
Alexis Moreno <jamorenov@concredito.com.mx>
2018-03-12 22:36:28 -0700

    se agrego todo lo necesario para kwic

--- src/main/java/Practica2.java
+++ src/main/java/Practica2.java
@@ -11,12 +11,9 @@ public class Practica2 {
     public static void main(String args []){
 
         try {
-            ArrayList<String> cadenas = new ArrayList<>();
-            ArrayList<String> lineas = getLinesOfFile("texto");
-            for ( String string : lineas ){
-                sortString(string,cadenas);
-            }
-            Collections.sort(cadenas,String.CASE_INSENSITIVE_ORDER);
+            ArrayList<Linea> lineas = getLinesOfFileTexto("texto.txt");
+            ArrayList<String> noClaves = getLinesOfFileNoClaves("noClaves.txt");
+            ArrayList<String> cadenas = implementKwic(lineas,noClaves);
             for ( String c : cadenas){
                 System.out.println(c);
             }
@@ -27,40 +24,30 @@ public class Practica2 {
 
     }
 
-    public static void sortString( String cadena, ArrayList<String> resultado ) {
-        String [] tokens = cadena.split("\\s");
-        resultado.add(cadena);
-        int tamano = tokens.length;
-        int contador = 1;
-        boolean bandera = true;
-        String inicio = tokens[0];
-        while (bandera){
-            String [] tokensAux = new String[tamano];
-            int n = 1;
-            for ( String s : tokens ){
-                tokensAux [n] = s;
-                n++;
-                if ( n == (tamano)){
-                    n = 0;
+    public static ArrayList<String> implementKwic( ArrayList<Linea> lineas, ArrayList<String> noClaves) {
+        ArrayList<String> cadenas = new ArrayList<>();
+        for ( Linea linea : lineas ){
+            Collections.sort(linea.getTokens(),String.CASE_INSENSITIVE_ORDER);
+            for ( String token : linea.getTokens()){
+                if ( noClaves.contains(token)){
+                    continue;
+                } else {
+                    String indice = token.concat("\t:\t\t");
+                    String enunciado = linea.getLinea().replace(token, "....");
+                    enunciado.concat("\n");
+                    indice += enunciado;
+                    cadenas.add(indice);
                 }
             }
-            String cadenaAux = tokensAux[0];
-            for ( int i=1; i < tamano; i++ ) {
-                cadenaAux += " " + tokensAux[i];
-            }
-            tokens = tokensAux;
-            resultado.add(cadenaAux);
-            if ( ++contador == tamano ){
-                bandera = false;
-            }
         }
-
+        return  cadenas;
     }
 
-    public static ArrayList<String> getLinesOfFile (String archivo ) throws IOException {
-        ArrayList<String> lineas = new ArrayList<>();
+    public static ArrayList<Linea> getLinesOfFileTexto (String archivo ) throws IOException {
+        ArrayList<Linea> lineas = new ArrayList<>();
+        ArrayList<String> tokens ;
         String cadena = "";
-
+        Linea linea;
         try {
             File file = new File(archivo);
             if (!file.exists()) {
@@ -73,12 +60,65 @@ public class Practica2 {
                 cadena = stripper.getText(documento);
                 String [] cadenas = cadena.split("\\r?\\n");
                 for ( String s : cadenas){
-                    lineas.add(s);
+                    linea = new Linea();
+                    tokens = new ArrayList<>();
+                    linea.setLinea(s);
+                    String [] palabras = s.split("\\s");
+                    for ( String palabra : palabras ){
+                        tokens.add(palabra);
+                    }
+                    linea.setTokens(tokens);
+                    lineas.add(linea);
+                }
+
+            } else {
+                BufferedReader bf = new BufferedReader(new FileReader( archivo ));
+                while ( (cadena = bf.readLine()) != null ) {
+                    linea = new Linea();
+                    tokens = new ArrayList<>();
+                    linea.setLinea(cadena);
+                    String [] palabras = cadena.split("\\s");
+                    for ( String palabra : palabras ){
+                        tokens.add(palabra);
+                    }
+                    linea.setTokens(tokens);
+                    lineas.add(linea);
+                }
+            }
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Practica2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lineas;
+    }
+
+    public static ArrayList<String> getLinesOfFileNoClaves (String archivo ) throws IOException {
+        ArrayList<String> lineas = new ArrayList<>();
+        String cadena = "";
+        try {
+            File file = new File(archivo);
+            if (!file.exists()) {
+                System.out.println("El archivo no existe");
+                System.exit(0);
+            }
+            if ( file.getName().endsWith(".pdf")){
+                PDDocument documento = PDDocument.load(file);
+                PDFTextStripper stripper = new PDFTextStripper();
+                cadena = stripper.getText(documento);
+                String [] cadenas = cadena.split("\\r?\\n");
+                for ( String s : cadenas){
+                    String [] tokens = s.split("\\s");
+                    for ( String token : tokens ){
+                        lineas.add(token);
+                    }
                 }
             } else {
                 BufferedReader bf = new BufferedReader(new FileReader( archivo ));
                 while ( (cadena = bf.readLine()) != null ) {
-                    lineas.add(cadena);
+                    String [] tokens = cadena.split("\\s");
+                    for ( String token : tokens ){
+                        lineas.add(token);
+                    }
                 }
             }
 
@@ -88,3 +128,27 @@ public class Practica2 {
         return lineas;
     }
 }
+    class Linea implements Comparable<Linea>{
+        private String linea;
+        private ArrayList<String> tokens;
+
+        public String getLinea() {
+            return linea;
+        }
+
+        public void setLinea(String linea) {
+            this.linea = linea;
+        }
+
+        public ArrayList<String>  getTokens() {
+            return tokens;
+        }
+
+        public void setTokens(ArrayList<String>  tokens) {
+            this.tokens = tokens;
+        }
+
+        public int compareTo(Linea l) {
+            return this.getLinea().compareTo(l.getLinea());
+        }
+    }
